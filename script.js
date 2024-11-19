49c6b8adbe2337d500043963feefa98d280faf76
mohinidangar <mohinidangar@gmail.com>
2024-11-19 19:23:28 +0530

    interactive

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Change background color for the About section when the button is clicked
 document.getElementById("change-bg-btn").addEventListener("click", function() {
     const aboutSection = document.getElementById("about");
-    const randomColor = `hsl(${Math.random() * 360}, 100%, 75%)`;
-    aboutSection.style.backgroundColor = randomColor;
+    const randomColor = `hsl(${Math.random() * 360}, 100%, 75%)`; // Random color
+    aboutSection.style.backgroundColor = randomColor; // Change background color
 });
+
 
