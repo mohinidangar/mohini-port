24f623935f5b7c8a8765bf8b42c1decd859f66a4
mohinidangar <mohinidangar@gmail.com>
2024-11-19 18:43:29 +0530

    changes

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple JavaScript to handle navigation smooth scrolling (optional)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
